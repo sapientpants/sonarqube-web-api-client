bfd2bcd99403ac424d30f1c5f08372c2b9ceb0ab
sapientpants <sapientpants@users.noreply.github.com>
2025-05-28 21:48:06 +0200

    feat: Add support for SonarQube Project Analyses API (#34)

---
+++ src/resources/project-analyses/index.ts
@@ -0,0 +1,18 @@
+export { ProjectAnalysesClient } from './ProjectAnalysesClient';
+export { SearchProjectAnalysesBuilder } from './builders';
+export type {
+  AnalysisEvent,
+  CreateEventRequest,
+  CreateEventResponse,
+  DeleteAnalysisRequest,
+  DeleteEventRequest,
+  EventCategory,
+  MutableEventCategory,
+  ProjectAnalysis,
+  SearchAnalysesRequest,
+  SearchAnalysesResponse,
+  SetBaselineRequest,
+  UnsetBaselineRequest,
+  UpdateEventRequest,
+  UpdateEventResponse,
+} from './types';
